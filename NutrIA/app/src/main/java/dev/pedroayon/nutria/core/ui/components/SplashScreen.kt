ca53b069b92afbd9e00eabb388d14a2bf8a3b7c1
PedroAyon <pedroayonb@gmail.com>
2025-05-08 15:06:34 -0600

    Splash screen simplified

--- NutrIA/app/src/main/java/dev/pedroayon/nutria/core/ui/components/SplashScreen.kt
+++ NutrIA/app/src/main/java/dev/pedroayon/nutria/core/ui/components/SplashScreen.kt
@@ -3,21 +3,15 @@ package dev.pedroayon.nutria.core.ui.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import dev.pedroayon.nutria.R
 
